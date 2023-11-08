d7637c27306d8ff5f55b20250ca2393fcb6938f4
Tushar Gahlaut <tushargahlaut74@gmail.com>
2023-11-08 21:39:18 +0530

    Completing Technical Skills

--- src/Utils/helper.ts
+++ src/Utils/helper.ts
@@ -69,6 +69,10 @@ export const workExperienceItems = [
         desc: "Followed Test Driven Development approach and used Jest, Mocha, Chai etc libraries to implement Test Suites for our microservices.",
       },
       {
+        id: 4,
+        desc: "Implemented Microsoft Azure AD Authentication for IndiGo's internal applications, also worked on Azure Blob storage and SAS tokens to manage document files.",
+      },
+      {
         id: 3,
         desc: "Utilised MySQL as a relational database management system and used Sequelize ORM in the microservices to interact seamlessly with the database.",
       },
@@ -189,7 +193,7 @@ export const technicalSkillsItems = [
       {
         id: 4,
         heading: "Next.js",
-        url: "https://w7.pngwing.com/pngs/87/586/png-transparent-next-js-hd-logo.png",
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Nextjs-logo.svg/2560px-Nextjs-logo.svg.png",
       },
       {
         id: 5,
@@ -200,6 +204,145 @@ export const technicalSkillsItems = [
         id: 6,
         heading: "Apollo GraphQL",
         url: "https://global.discourse-cdn.com/business5/uploads/apollographql/original/1X/25bd5104d61020fe4dc0777a5919cd009bca633e.png",
+      },
+      {
+        id: 7,
+        heading: "Sequelize",
+        url: "https://miro.medium.com/v2/0*rwd6KeolcXgz7zpx.png",
+      },
+      {
+        id: 8,
+        heading: "Mongoose",
+        url: "https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/mongoose/mongoose.png",
+      },
+    ],
+  },
+  {
+    id: 2,
+    heading: "Databases",
+    items: [
+      {
+        id: 0,
+        heading: "MongoDB",
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/MongoDB_Logo.svg/1280px-MongoDB_Logo.svg.png",
+      },
+      {
+        id: 1,
+        heading: "MySQL",
+        url: "https://upload.wikimedia.org/wikipedia/mg/thumb/6/62/MySQL.svg/1280px-MySQL.svg.png",
+      },
+      {
+        id: 2,
+        heading: "SQLite3",
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Sqlite-square-icon.svg/1200px-Sqlite-square-icon.svg.png",
+      },
+      {
+        id: 3,
+        heading: "Azure Blob Storage",
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Microsoft_Azure.svg/1200px-Microsoft_Azure.svg.png",
+      },
+    ],
+  },
+  {
+    id: 3,
+    heading: "DevOps and Cloud",
+    items: [
+      {
+        id: 0,
+        heading: "Microsoft Azure",
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Microsoft_Azure.svg/1200px-Microsoft_Azure.svg.png",
+      },
+      {
+        id: 1,
+        heading: "Docker",
+        url: "https://miro.medium.com/v2/resize:fit:400/1*OARpkeBkn_Tw3vk8H769OQ.png",
+      },
+      {
+        id: 2,
+        heading: "Kubernetes",
+        url: "https://www.ovhcloud.com/sites/default/files/styles/text_media_horizontal/public/2021-04/K8S-logo.png",
+      },
+      {
+        id: 3,
+        heading: "CI/CD Pipelines",
+        url: "https://www.mabl.com/hubfs/CICDBlog.png",
+      },
+      {
+        id: 4,
+        heading: "Tekton",
+        url: "https://cd.foundation/wp-content/uploads/sites/78/2020/04/tekton-icon-color-1-1.png",
+      },
+      {
+        id: 5,
+        heading: "Red Hat Openshift",
+        url: "https://upload.wikimedia.org/wikipedia/commons/3/3a/OpenShift-LogoType.svg",
+      },
+    ],
+  },
+  {
+    id: 4,
+    heading: "API and Development Tools",
+    items: [
+      {
+        id: 0,
+        heading: "REST",
+        url: "https://cdn-icons-png.flaticon.com/512/10329/10329422.png",
+      },
+      {
+        id: 1,
+        heading: "GraphQL",
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/GraphQL_Logo.svg/2048px-GraphQL_Logo.svg.png",
+      },
+      {
+        id: 2,
+        heading: "Visual Studio Code",
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/2048px-Visual_Studio_Code_1.35_icon.svg.png",
+      },
+      {
+        id: 3,
+        heading: "Postman",
+        url: "https://upload.wikimedia.org/wikipedia/commons/c/c2/Postman_%28software%29.png",
+      },
+      {
+        id: 4,
+        heading: "GitHub",
+        url: "https://upload.wikimedia.org/wikipedia/commons/c/c2/GitHub_Invertocat_Logo.svg",
+      },
+      {
+        id: 5,
+        heading: "Jira",
+        url: "https://assets.project-management.com/uploads/2022/12/atlassian_jira.png",
+      },
+    ],
+  },
+  {
+    id: 5,
+    heading: "Core CS Subjects",
+    items: [
+      {
+        id: 0,
+        heading: "Data Structures and Algorithms",
+        url: "https://e7.pngegg.com/pngimages/78/465/png-clipart-data-structures-and-algorithms-algorithms-data-structures-programs-computer-science-others-miscellaneous-computer-science-thumbnail.png",
+      },
+      {
+        id: 1,
+        heading: "Operating Systems",
+        url: "https://cdn-icons-png.flaticon.com/512/2172/2172894.png",
+      },
+      {
+        id: 2,
+        heading: "Computer Networks",
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Server-based-network.svg/743px-Server-based-network.svg.png",
+      },
+      {
+        id: 3,
+        heading: "Database Management Systems",
+        url: "https://w7.pngwing.com/pngs/310/475/png-transparent-database-computer-icons-computer-software-information-database-miscellaneous-angle-information-technology.png",
+      },
+      {
+        id: 4,
+        heading: "Object Oriented Programming",
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/OOP.svg/1200px-OOP.svg.png",
       },
     ],
   },
